a8ed5b852dc624b49739eb46b6dc2f717bb8418b
cienco <157473772+cienco@users.noreply.github.com>
2025-09-24 14:57:57 +0200

    Update database.py

--- src/mcp_server_motherduck/database.py
+++ src/mcp_server_motherduck/database.py
@@ -3,9 +3,9 @@ from .configs import DB_PATH, MOTHERDUCK_TOKEN, DEMO_RW
 
 def connect():
     """
-    Crea una connessione a MotherDuck.
+    Connessione a MotherDuck/DuckDB.
     - read_only = True (prod) / False (demo RW)
-    - imposta il token MotherDuck
+    - imposta il token MotherDuck se presente
     """
     con = duckdb.connect(database=DB_PATH, read_only=not DEMO_RW)
     if MOTHERDUCK_TOKEN:
